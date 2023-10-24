4b4a321f8ad0e5cc53623c3d194a5202fc8b818b
Lisandro Cordoba <lisandrocordoba11@gmail.com>
2023-10-24 00:27:37 -0300

    eliminar (mal)

--- Talleres/taller4/estudiantes/src/main/java/aed/ABB.java
+++ Talleres/taller4/estudiantes/src/main/java/aed/ABB.java
@@ -44,7 +44,7 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
         if (desde == null){ 
             return null; // Si el arbol está vacio devuelve NULL
         } else if (desde.value == elem || (desde.right == null && is_greater) || (desde.left == null && !(is_greater))){
-            return desde; // Devuelve el nodo del elemento. Si no está devuelve el que será su padre
+            return desde; // Devuelve el nodo del elemento. Si no está devuelve el que sería (si se quiere agregar elem) su padre
         } else if (elem.compareTo(desde.value) > 0){
             return buscar_nodo(desde.right, elem);
         } else {
@@ -123,6 +123,8 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
         return (!(searched == null || searched.value != elem));
     }
 
+
+
     /*
     private boolean busqueda_recursiva(Nodo desde, T elem){
         Nodo next;
@@ -139,11 +141,94 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
             return busqueda_recursiva(next, elem);
         }
     }
- */
+
+    private Nodo buscar_nodo(Nodo desde, T elem){
+    Boolean is_greater = (desde != null && (elem.compareTo(desde.value) > 0));
+    if (desde == null){ 
+        return null; // Si el arbol está vacio devuelve NULL
+    } else if (desde.value == elem || (desde.right == null && is_greater) || (desde.left == null && !(is_greater))){
+        return desde; // Devuelve el nodo del elemento. Si no está devuelve el que sería (si se quiere agregar elem) su padre
+    } else if (elem.compareTo(desde.value) > 0){
+        return buscar_nodo(desde.right, elem);
+    } else {
+        return buscar_nodo(desde.left, elem);
+    }
+}
+
     public void eliminar(T elem){
-        
+        eliminar_recursivo(_origin, elem);
     }
 
+    public void eliminar_recursivo(Nodo desde, T elem){
+        if(desde != null){ // Caso base es desde == null y no hace nada
+            if (desde.value == elem){
+                if ()
+            }
+            Boolean is_greater = (elem.compareTo(desde.value) > 0);
+
+        }
+    }
+ */
+
+
+    public void eliminar(T elem){
+        // VER ELIMINAR RAIZ
+        Nodo to_delete = buscar_nodo(_origin, elem);
+        if (to_delete != null){
+            int right_left = elem.compareTo(to_delete.father.value); //( r_l > 0 si elem > su padre) / (r_l < 0 si elem < su padre)
+            if (to_delete.right != null && to_delete.left != null){ // CASO 3
+                if (right_left > 0){
+                    Nodo sucessor = search_successor(to_delete);
+                    
+                } else {
+                    Nodo predecessor = search_predecessor(to_delete);
+
+                }     
+            } else if (to_delete.right != null){ // CASO 2
+                if (right_left > 0){
+                    (to_delete.father).right = to_delete.right;
+                    (to_delete.right).father = to_delete.father;
+                } else {
+                    (to_delete.father).left = to_delete.right;
+                    (to_delete.right).father = to_delete.father;
+                }     
+            } else if (to_delete.left != null){ // CASO 2
+                if (right_left > 0){
+                    (to_delete.father).right = to_delete.left;
+                    (to_delete.left).father = to_delete.father;
+                } else {
+                    (to_delete.father).left = to_delete.left;
+                    (to_delete.left).father = to_delete.father;
+                }     
+            } else { // CASO 1
+                if (right_left > 0){
+                    (to_delete.father).right = null;
+                } else {
+                    to_delete.father.left = null;
+                }
+                to_delete.father = null; /// VER ALIASING
+            }
+            _cardinal --;
+            //////////// VER MAXIMOS Y MINIMOS!!!!!
+        }
+    }
+
+    private Nodo search_succesor(Nodo actual){
+        actual = actual.right;
+        while(actual.left != null){
+            actual = actual.left; /// VER ALIASING
+        }
+        return actual;
+    }
+
+    private Nodo search_predecesor(Nodo actual){
+        actual = actual.left;
+        while(actual.right != null){
+            actual = actual.right; /// VER ALIASING
+        }
+        return actual;
+    }
+ 
     public String toString(){
         throw new UnsupportedOperationException("No implementada aun");
     }
